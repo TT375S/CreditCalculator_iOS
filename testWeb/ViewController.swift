92716bc903cb8718b9987004edd0c596d871981b
TT375S <tomataka375@gmail.com>
2017-02-06 10:26:05 +0900

    add comment

--- testWeb/ViewController.swift
+++ testWeb/ViewController.swift
@@ -33,13 +33,6 @@ class ViewController: UIViewController, UIWebViewDelegate  {
         
         // リクエストを投げる
         webView.loadRequest(request)
-        
-        var html = webView.stringByEvaluatingJavaScript(from: "document.body.innerHTML")
-        print(html as Any)
-        print("hello")
-        
-        html = webView.stringByEvaluatingJavaScript(from: "document.body.innerHTML")
-        print(html as Any)
     }
     
     
@@ -49,19 +42,27 @@ class ViewController: UIViewController, UIWebViewDelegate  {
         print("indicator on")
     }
     
-    // ロード完了でインジケータ非表示
+    // ロード完了
     func webViewDidFinishLoad(_ webView: UIWebView) {
+        //インジケータ非表示
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
         print("indicator off")
         print("END")
+        //iframeとかだと複数回呼ばれるので、一回だけ実行するためにisLoadingを確認する。
         if !webView.isLoading {
+            //URL表示
             print(webView.stringByEvaluatingJavaScript(from: "document.URL") as Any)
+            //タイトルをナビゲーションのタイトルにする
             self.navigationItem.title = webView.stringByEvaluatingJavaScript(from: "document.title")
+            //htmlソースを表示
+            var html = webView.stringByEvaluatingJavaScript(from: "document.body.innerHTML")
+            print(html as Any)
+            print("hello")
+            //ログイン後の画面ならjavascriptのdoSubmit関数をオーバーライドして同じウィンドウ(?)で開く
             if webView.stringByEvaluatingJavaScript(from: "document.URL") == "https://coursereg.waseda.jp/portal/simpleportal.php?HID_P14=EN"{
                     print("HITT")
                 
                         webView.stringByEvaluatingJavaScript(from: "function doSubmit ( strURL, strAuthGp, strAuth, strStatus, strWindow ) {strWindow='_self';intRtnVal=fncControlSubmit ( 10 );if ( intRtnVal == true ){window.open( '','_self','menubar=no,status=yes,scrollbars=yes,location=no,resizable=yes' );document.F01.url.value=strURL;document.F01.HID_P6.value=strAuthGp;document.F01.HID_P8.value=strAuth;document.F01.target='ap';document.F01.pageflag.value=1000;document.F01.status.value=strStatus;document.F01.target='_self';document.F01.submit();} else {alert ( 'ただいま処理中です。OKボタンを押して、しばらく待ってから再度メニューをクリックしてください。' );}}")
-//                                        webView.stringByEvaluatingJavaScript(from: "function doSubmit( strURL, strAuthGp, strAuth, strStatus, strWindow ){strWindow = '_self';intRtnVal=fncControlSubmit ( 10 ); if ( intRtnVal == true ) {window.open ( '', strWindow, 'menubar=no,status=yes,scrollbars=yes,location=no,resizable=yes' );document.F01.url.value=strURL;document.F01.HID_P6.value=strAuthGp;document.F01.HID_P8.value=strAuth;document.F01.target='ap'document.F01.pageflag.value=1000;document.F01.status.value=strStatus;document.F01.target='_self';document.F01.submit();} else {alert ( 'ただいま処理中です。OKボタンを押して、しばらく待ってから再度メニューをクリックしてください。' );}}}")
                 
                 //とりあえずこの下のやつをやれば成績画面は出る。
 //                    webView.stringByEvaluatingJavaScript(from: "doSubmit('https://www.wnp12.waseda.jp/kyomu/epb2050.htm', 'eStudent', 'ea02', '1', '_self');")
